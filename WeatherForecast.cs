fcc140eb86402d74a7e643cc917903ad2588f1e2
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-20 10:19:29 -0400

    add asp.net webapi project

---
+++ WeatherForecast.cs
@@ -0,0 +1,14 @@
+// This is a template class provided by Microsoft
+
+namespace spikewall;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
