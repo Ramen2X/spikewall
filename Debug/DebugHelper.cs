ceed5b4cb9a606e43c0398ccebd54cb8aa0389ac
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-20 17:47:46 -0400

    color code debug output

--- Debug/DebugHelper.cs
+++ Debug/DebugHelper.cs
@@ -6,13 +6,19 @@
         {
             switch (type) {
                 case (1):
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.WriteLine("[Game] " + text);
+                    Console.ResetColor();
                     break;
                 case (2):
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("![Warn]! " + text);
+                    Console.ResetColor();
                     break;
                 default:
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("[Server] " + text);
+                    Console.ResetColor();
                     break;
             }
         }
