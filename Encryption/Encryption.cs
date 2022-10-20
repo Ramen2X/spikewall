009374fb5a8514a06d729d713f8c0f6415a92310
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-20 13:02:36 -0400

    add encrypt method to encryption class

--- Encryption/Encryption.cs
+++ Encryption/Encryption.cs
@@ -10,7 +10,7 @@ namespace spikewall.Encryption
     {
         static readonly string m_encryptionKey = "Ec7bLaTdSuXuf5pW";
         static readonly byte[] m_encryptionKeyByte = System.Text.Encoding.UTF8.GetBytes(m_encryptionKey);
-        static readonly string m_encryptionIV = "burgersMetKortin";
+        static readonly string m_encryptionIV = "burgersMetKortin"; // Temporary, the client is the one who gives us the decryption IV
         static readonly byte[] m_encryptionIVByte = System.Text.Encoding.UTF8.GetBytes(m_encryptionIV);
 
         /// <summary>
@@ -38,6 +38,37 @@ namespace spikewall.Encryption
             decryptedOutput = sr.ReadToEnd();
 
             return decryptedOutput;
+        }
+
+        /// <summary>
+        /// AES encrypts a given string using the encryption key and IV.
+        /// </summary>
+        /// <param name="input">String to be encrypted.</param>
+        /// <returns>A Base64-encoded and AES encrypted string.</returns>
+        public static string Encrypt(string input)
+        {
+            string encryptedOutput;
+            byte[] encryptedByte;
+
+            Aes aes = Aes.Create();
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = m_encryptionKeyByte;
+            aes.IV = m_encryptionIVByte;
+
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using MemoryStream ms = new();
+            using CryptoStream cs = new(ms, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter sw = new(cs)) {
+                sw.Write(input);
+            }
+
+            encryptedByte = ms.ToArray();
+            encryptedOutput = Convert.ToBase64String(encryptedByte);
+
+            return encryptedOutput;
         }
     }
 }
