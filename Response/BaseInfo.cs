378498c58310b7a8423c12eb913a25ebb54eea55
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-20 15:04:07 -0400

    begin work on BaseResponse/BaseInfo

---
+++ Response/BaseInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace spikewall.Response
+{
+    public class BaseInfo
+    {
+        string m_errorMessage;
+        string m_closeTime;
+        string m_seq;
+        string m_serverTime;
+        string m_statusCode;
+
+        /// <summary>
+        /// The constructors for BaseInfo. If no parameters are specified, the default will be used.
+        /// </summary>
+        /// <param name="em">The error message.</param>
+        /// <param name="ct">The close time.</param>
+        /// <param name="s">The "seq". (honestly I have no idea what this is)</param>
+        /// <param name="sc">The status code.</param>
+        public BaseInfo(string em, string ct, string s, string sc)
+        {
+            m_errorMessage = em;
+            m_closeTime = ct;
+            m_seq = s;
+            m_serverTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            m_statusCode = sc;
+        }
+
+        public BaseInfo()
+        {
+            m_errorMessage = "OK";
+            m_closeTime = DateTime.Now.AddTicks(-1).AddDays(1).ToString();
+            m_seq = "0";
+            m_serverTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            m_statusCode = "0";
+        }
+    }
+}
---
+++ Response/BaseResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace spikewall.Response
+{
+    /// <summary>
+    /// Base response class from which all other responses are derived from.
+    /// </summary>
+    public class BaseResponse
+    {
+        BaseInfo m_bi;
+        string m_assetsVersion;
+        string m_clientDataVersion;
+        string m_dataVersion;
+        string m_infoVersion;
+        string m_version;
+        string m_spikewallVersion;
+
+        /// <summary>
+        /// The constructors for a BaseResponse. If no parameters are specified, the default will be used.
+        /// </summary>
+        /// <param name="bi">The BaseInfo object.</param>
+        /// <param name="av">The assets version.</param>
+        /// <param name="cdv">The client data version.</param>
+        /// <param name="dv">The data version.</param>
+        /// <param name="iv">The info version.</param>
+        /// <param name="v">The version.</param>
+        /// <param name="swv">The spikewall version.</param>
+        public BaseResponse(BaseInfo bi, string av, string cdv, string dv, string iv, string v, string swv)
+        {
+            m_bi = bi;
+            m_assetsVersion = av;
+            m_clientDataVersion = cdv;
+            m_dataVersion = dv;
+            m_infoVersion = iv;
+            m_version = v;
+            m_spikewallVersion = swv;
+        }
+
+        public BaseResponse()
+        {
+            m_bi = new BaseInfo();
+            m_assetsVersion = "054";
+            m_clientDataVersion = "2.2.2";
+            m_dataVersion = "15";
+            m_infoVersion = "017";
+            m_version = "2.2.2";
+            m_spikewallVersion = "0.0.1";
+        }
+    }
+}
