5a5058e204078ace73d76c426df846f2acd94898
F121Live <55333985+F121Live@users.noreply.github.com>
2022-10-20 18:36:52 -0300

    Set up config object and return it

---
+++ Config/Config.cs
@@ -0,0 +1,66 @@
+namespace spikewall.Config
+{
+    // Config Object
+    public class Current
+    {
+            int port = 9001;
+            string dbName = "spikewall";
+            string dbPassword = "default";
+            string dbUser = "root";
+            int dbPort = 3306;
+            bool debugLog;
+            bool isMaintenance;
+            bool supportLegacyVersions;
+
+            public int Port
+            {
+                get { return port; }
+                set { port = value; }
+            }
+
+            public string DbName
+            {
+                get { return dbName; }
+                set { dbName = value; }
+            }
+
+            public string DbPassword
+            {
+                get { return dbPassword; }
+                set { dbPassword = value; }
+            }
+
+            public string DbUser
+            {
+                get { return dbUser; }
+                set { dbUser = value; }
+            }
+
+            public int DbPort
+            {
+                get { return port; }
+                set { port = value; }
+            }
+
+            public bool IsMaintenance
+            {
+                get { return isMaintenance; }
+                set { isMaintenance = value; }
+            }
+            public bool SupportLegacyVersions
+            {
+                get { return supportLegacyVersions; }
+                set { supportLegacyVersions = value; }
+            }
+    }
+    
+    public static class Set
+    {
+        // Create a new config object and return it, TODO: take values from a config file
+        public static Current Config()
+        {
+            var config = new Config.Current();
+            return config;
+        }
+    }
+}
