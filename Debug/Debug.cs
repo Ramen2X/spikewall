b347b0fc83acba009500cfbb7d3befce0f3c224a
F121Live <55333985+F121Live@users.noreply.github.com>
2022-10-20 17:11:13 -0300

    Add simple logger

---
+++ Debug/Debug.cs
@@ -0,0 +1,21 @@
+﻿namespace spikewall.Debug
+{
+    public class Debug
+    {
+        public static void Log(string text, int type = 0)
+        {
+            switch (type)
+            {
+                case (1):
+                    Console.WriteLine("[Game] " + text);
+                    break;
+                case (2):
+                    Console.WriteLine("![Warn]! " + text);
+                    break;
+                default:
+                    Console.WriteLine("[Server] " + text);
+                    break;
+            }
+        }
+    }
+}
