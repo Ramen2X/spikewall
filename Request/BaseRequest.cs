1797c978d143778285c12d058b67e78aefa0369d
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-21 15:56:14 -0400

    whoops versionid should just be version

--- Request/BaseRequest.cs
+++ Request/BaseRequest.cs
@@ -6,7 +6,7 @@
     public class BaseRequest
     {
         public string SessionID { get; set; }
-        public string VersionID { get; set; }
+        public string Version { get; set; }
         public string RevivalVerID { get; set; }
         public string Seq { get; set; }
     }
