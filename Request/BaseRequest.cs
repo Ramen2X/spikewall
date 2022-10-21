185c6f1d007c4a21080e63d71a87c97eec397d15
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-21 15:54:25 -0400

    begin work on Base/LoginRequest

---
+++ Request/BaseRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace spikewall.Request
+{
+    /// <summary>
+    /// Class from which requests are derived from.
+    /// </summary>
+    public class BaseRequest
+    {
+        public string SessionID { get; set; }
+        public string VersionID { get; set; }
+        public string RevivalVerID { get; set; }
+        public string Seq { get; set; }
+    }
+}
---
+++ Request/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace spikewall.Request
+{
+    /// <summary>
+    /// Class to store data from a login request.
+    /// </summary>
+    public class LoginRequest : BaseRequest
+    {
+        public string Version { get; set; }
+        public string Device { get; set; }
+        public string Platform { get; set; }
+        public string Language { get; set; }
+        public string SalesLocate { get; set; }
+        public string StoreId { get; set; }
+        public string Platform_sns { get; set; }
+    }
+}
