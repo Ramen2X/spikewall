afefdb055f6bb9bb06b48c7502da33b9b020a577
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2022-10-20 17:06:42 -0700

    improve documentation on Db class

--- Db/Db.cs
+++ Db/Db.cs
@@ -5,17 +5,20 @@ namespace spikewall
 {
     public class Db
     {
+        /// <summary>
+        /// Initialize database details from locally-stored secrets
+        /// </summary>
+        /// MySQL connection details are stored as .NET secrets. To set these on the server (or
+        /// locally), issue the following commands (replacing the second value as necessary):
+        ///
+        ///   dotnet user-secrets set "Db:Host" "localhost"
+        ///   dotnet user-secrets set "Db:Port" "3306"
+        ///   dotnet user-secrets set "Db:Username" "dbuser"
+        ///   dotnet user-secrets set "Db:Password" "dbpass"
+        ///   dotnet user-secrets set "Db:Database" "spikewall"
+        ///
         public static void Initialize(ref WebApplicationBuilder builder)
         {
-            // MySQL connection details are stored as .NET secrets. To set these on the server (or
-            // locally), issue the following commands (replacing the second value as necessary):
-            //
-            //   dotnet user-secrets set "Db:Host" "localhost"
-            //   dotnet user-secrets set "Db:Port" "3306"
-            //   dotnet user-secrets set "Db:Username" "dbuser"
-            //   dotnet user-secrets set "Db:Password" "dbpass"
-            //   dotnet user-secrets set "Db:Database" "spikewall"
-
             dbHost = builder.Configuration["Db:Host"];
             dbUser = builder.Configuration["Db:Username"];
             dbPass = builder.Configuration["Db:Password"];
@@ -23,14 +26,26 @@ namespace spikewall
             dbName = builder.Configuration["Db:Database"];
         }
 
+        /// <summary>
+        /// Retrieve valid MySQL connection to make queries with.
+        /// </summary>
+        ///
+        /// This function will return a valid MySqlConnection, or pass a MySqlException if an error
+        /// occurs. Using try/catch is recommended, at least in sections where the validity of
+        /// database details is checked.
+        ///
+        /// This can also be used in a `using` statement, e.g. `using (var conn = Db.Get())`
+        ///
+        /// After calling this function, call `Open()` on the resulting object. Then queries can be
+        /// made with MySqlCommand. Call `Close()` when you're finished (this might happen
+        /// automatically with `using`, but it's probably good practice either way),
         public static MySqlConnection Get()
         {
-            // Build MySQL connection string
+            // Build MySQL connection string out of loaded parameters
             string connectionString = String.Format("server={0};user={1};database={2};port={3};password={4}",
                                                     dbHost, dbUser, dbName, dbPort, dbPass);
 
-            // Return MySqlConnection based on connection string. Note that if the details are wrong
-            // or missing, this has a chance of throwing a MySqlException.
+            // Return connection
             return new MySqlConnection(connectionString);
         }
 
