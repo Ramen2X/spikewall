c49fa2c4fea0f2520ddc9cf7dd5da50874a24627
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-20 21:02:53 -0400

    begin work on LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using spikewall.Debug;
+
+namespace spikewall.Controllers
+{
+    [ApiController]
+    [Route("/login/Login/")]
+    [Produces("text/json")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost(Name = "Login")]
+        public void Login()
+        {
+
+        }
+    }
+}
