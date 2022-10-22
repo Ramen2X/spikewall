f990afbdb4dd761b87f9ae872c1dbdcf49ea2f82
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-21 21:37:43 -0400

    rework responses to work with serialization

--- Response/BaseInfo.cs
+++ Response/BaseInfo.cs
@@ -2,11 +2,11 @@
 {
     public class BaseInfo
     {
-        public string m_errorMessage;
-        public string m_closeTime;
-        public string m_seq;
-        public string m_serverTime;
-        public string m_statusCode;
+        public string errorMessage { get; set; }
+        public string closeTime { get; set; }
+        public string seq { get; set; }
+        public string server_time { get; set; }
+        public string statusCode { get; set; }
 
         /// <summary>
         /// The constructors for BaseInfo. If no parameters are specified, the default will be used.
@@ -17,20 +17,20 @@
         /// <param name="sc">The status code.</param>
         public BaseInfo(string em, string ct, string s, string sc)
         {
-            m_errorMessage = em;
-            m_closeTime = ct;
-            m_seq = s;
-            m_serverTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
-            m_statusCode = sc;
+            errorMessage = em;
+            closeTime = ct;
+            seq = s;
+            server_time = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            statusCode = sc;
         }
 
         public BaseInfo()
         {
-            m_errorMessage = "OK";
-            m_closeTime = DateTime.Now.AddTicks(-1).AddDays(1).ToString();
-            m_seq = "0";
-            m_serverTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
-            m_statusCode = "0";
+            errorMessage = "OK";
+            closeTime = DateTime.Now.AddTicks(-1).AddDays(1).ToString();
+            seq = "0";
+            server_time = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            statusCode = "0";
         }
     }
 }
--- Response/BaseResponse.cs
+++ Response/BaseResponse.cs
@@ -5,47 +5,43 @@ namespace spikewall.Response
     /// <summary>
     /// Base response class from which all other responses are derived from.
     /// </summary>
-    public class BaseResponse
+    public class BaseResponse : BaseInfo
     {
-        public BaseInfo m_bi;
-        public string m_assetsVersion;
-        public string m_clientDataVersion;
-        public string m_dataVersion;
-        public string m_infoVersion;
-        public string m_version;
-        public string m_spikewallVersion;
+        public string assets_version { get; set; }
+        public string client_data_version { get; set; }
+        public string data_version { get; set; }
+        public string info_version { get; set; }
+        public string version { get; set; }
+        public string spikewall_version { get; set; }
 
         /// <summary>
         /// The constructors for a BaseResponse. If no parameters are specified, the default will be used.
         /// </summary>
-        /// <param name="bi">The BaseInfo object.</param>
         /// <param name="av">The assets version.</param>
         /// <param name="cdv">The client data version.</param>
         /// <param name="dv">The data version.</param>
         /// <param name="iv">The info version.</param>
         /// <param name="v">The version.</param>
         /// <param name="swv">The spikewall version.</param>
-        public BaseResponse(BaseInfo bi, string av, string cdv, string dv, string iv, string v, string swv)
+        public BaseResponse(string av, string cdv, string dv, string iv, string v, string swv)
         {
-            m_bi = bi;
-            m_assetsVersion = av;
-            m_clientDataVersion = cdv;
-            m_dataVersion = dv;
-            m_infoVersion = iv;
-            m_version = v;
-            m_spikewallVersion = swv;
+            assets_version = av;
+            client_data_version = cdv;
+            data_version = dv;
+            info_version = iv;
+            version = v;
+            spikewall_version = swv;
         }
 
         // TODO: Get these defaults from the config
         public BaseResponse()
         {
-            m_bi = new BaseInfo();
-            m_assetsVersion = "054";
-            m_clientDataVersion = "2.2.2";
-            m_dataVersion = "15";
-            m_infoVersion = "017";
-            m_version = "2.2.2";
-            m_spikewallVersion = "0.0.1";
+            assets_version = "054";
+            client_data_version = "2.2.2";
+            data_version = "15";
+            info_version = "017";
+            version = "2.2.2";
+            spikewall_version = "0.0.1";
         }
     }
 }
