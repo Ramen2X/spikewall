cb218795f31c17e15050ef80801c69eb19266675
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-24 21:43:23 -0400

    add campaign object

---
+++ Object/Campaign.cs
@@ -0,0 +1,11 @@
+﻿namespace spikewall.Object
+{
+    public class Campaign
+    {
+        public Int64? campaignType { get; set; }
+        public Int64? campaignContent { get; set; }
+        public Int64? campaignSubContent { get; set; }
+        public Int64? campaignStartTime { get; set; }
+        public Int64? campaignEndTime { get; set; }
+    }
+}
