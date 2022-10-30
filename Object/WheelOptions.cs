a4ebc260fda191ec18ada8c5304414917819a999
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-30 17:39:19 -0400

    implement WheelOptions object

---
+++ Object/WheelOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace spikewall.Object
+{
+    public class WheelOptions
+    {
+        public string[]? items { get; set; }
+        public long[]? item { get; set; }
+        public long[]? itemWeight { get; set; }
+        public long? itemWon { get; set; }
+        public long? nextFreeSpin { get; set; }
+        public long? spinCost { get; set; }
+        public long? rouletteRank { get; set; }
+        public long? numRouletteToken { get; set; }
+        public long? numJackpotRing { get; set; }
+        public long? numRemainingRoulette { get; set; }
+        public Item[]? itemList { get; set; }
+    }
+}
