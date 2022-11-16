4e39a2ef285d8781ab55161d54a525839ea82c3f
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-11-16 16:52:19 -0500

    add item ID enum

--- Object/Item.cs
+++ Object/Item.cs
@@ -5,4 +5,28 @@
         public long? itemId { get; set; }
         public long? numItem { get; set; }
     }
+
+    /// <summary>
+    /// Enum that contains the names 
+    /// and IDs of all relevant items.
+    /// </summary>
+    public enum ItemID
+    {
+        None = -1,
+
+        Invincible = 120000,
+        Shield,
+        Magnet,
+        Trampoline,
+        Combo,
+        Laser,
+        Drill,
+        Asteroid,
+        RingBonus,
+        DistanceBonus,
+        AnimalBonus,
+
+        RedStarRing = 900000,
+        Ring = 910000
+    }
 }
