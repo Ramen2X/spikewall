1da30f54059bffd3e6c651767fd246564c624b18
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2022-11-18 13:49:41 -0800

    move chaostate population to its own function

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -90,56 +90,14 @@ namespace spikewall.Controllers
                 return new JsonResult(EncryptedResponse.Generate(iv, clientReq.error));
             }
 
-            // Get list of all visible chao
-            var command = new MySqlCommand("SELECT * FROM `sw_chao`;", conn);
-
-            List<Chao> chao = new List<Chao>();
-
-            using (var chaoRdr = command.ExecuteReader())
+            Chao[] chaoState;
+            SRStatusCode chaoStateStatus = Chao.PopulateChaoState(conn, clientReq.userId, out chaoState);
+            if (chaoStateStatus != SRStatusCode.Ok)
             {
-                while (chaoRdr.Read()) {
-                    Chao c = new Chao();
-
-                    c.chaoID = chaoRdr.GetString("id");
-                    c.rarity = Convert.ToInt64(chaoRdr["rarity"]);
-                    c.hidden = Convert.ToInt64(chaoRdr["hidden"]);
-
-                    chao.Add(c);
-                }
-
-                chaoRdr.Close();
+                return new JsonResult(EncryptedResponse.Generate(iv, chaoStateStatus));
             }
 
-            for (int i = 0; i < chao.Count; i++) {
-                Chao c = chao[i];
-
-                var sql = Db.GetCommand("SELECT * FROM `sw_chaostates` WHERE user_id = '{0}' AND chao_id = '{1}';", clientReq.userId, c.chaoID);
-                var stateCmd = new MySqlCommand(sql, conn);
-                var stateRdr = stateCmd.ExecuteReader();
-
-                if (stateRdr.HasRows) {
-                    // Read row
-                    stateRdr.Read();
-
-                    c.status = Convert.ToSByte(stateRdr["status"]);
-                    c.level = Convert.ToSByte(stateRdr["level"]);
-                    c.setStatus = Convert.ToInt64(stateRdr["set_status"]);
-                    c.acquired = Convert.ToInt64(stateRdr["acquired"]);
-
-                    stateRdr.Close();
-                } else {
-                    stateRdr.Close();
-
-                    // Insert a default chaostate
-                    sql = Db.GetCommand(@"INSERT INTO `sw_chaostates` (chao_id, user_id) VALUES ('{0}', '{1}');", c.chaoID, clientReq.userId);
-                    var insertCmd = new MySqlCommand(sql, conn);
-                    insertCmd.ExecuteNonQuery();
-                }
-            }
-
-            conn.Close();
-
-            return new JsonResult(EncryptedResponse.Generate(iv, new ChaoStateResponse(chao.ToArray())));
+            return new JsonResult(EncryptedResponse.Generate(iv, new ChaoStateResponse(chaoState)));
         }
 
         [HttpPost]
